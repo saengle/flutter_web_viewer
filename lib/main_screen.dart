eb541899f672c331c2737caf42d76f4301a64c5c
saengle <zohv817@gmail.com>
2023-01-30 14:50:16 +0900

    Add: 옵션 메뉴

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -22,6 +22,28 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('main'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              print(value);
+            },
+            itemBuilder: (context) => [
+              PopupMenuItem<String>(
+                value: 'https://google.com',
+                child: Text('구글'),
+              ),
+              PopupMenuItem<String>(
+                value: 'https://naver.com',
+                child: Text('네이버'),
+              ),
+              PopupMenuItem<String>(
+                value: 'https://kakao.com',
+                child: Text('카카오'),
+              ),
+            ],
+          ),
+        ],
       ),
       body: WebView(
         initialUrl: 'https://flutter.dev',
